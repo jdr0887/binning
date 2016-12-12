6180d9f1a4b7bf7aadb8b91b9ec7fd3132d47f6a
Jason Reilly <jdr0887@gmail.com>
2016-12-12 09:34:30 -0500

    returning Response

--- binning-workflows/binning-workflow-diagnostic-gs/binning-workflow-diagnostic-gs-ws/src/main/java/org/renci/binning/diagnostic/gs/ws/DiagnosticGeneScreenService.java
+++ binning-workflows/binning-workflow-diagnostic-gs/binning-workflow-diagnostic-gs-ws/src/main/java/org/renci/binning/diagnostic/gs/ws/DiagnosticGeneScreenService.java
@@ -5,14 +5,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/DiagnosticGeneScreenService/")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 public interface DiagnosticGeneScreenService {
 
     @POST
-    @Path("/submit/")
-    public Integer submit(String participant, String gender, Integer dxId, Integer listVersion);
+    @Path("/submit")
+    public Response submit(DiagnosticBinningJobInfo info);
 
 }
