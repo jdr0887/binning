5926569c988e592101d69ef25601e8b4b2e420d8
Jason Reilly <jdr0887@gmail.com>
2016-12-12 09:30:24 -0500

    fixing identifier

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/PhenotypeXDAO.java
+++ binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/PhenotypeXDAO.java
@@ -3,6 +3,6 @@ package org.renci.binning.dao.clinbin;
 import org.renci.binning.dao.BaseDAO;
 import org.renci.binning.dao.clinbin.model.PhenotypeX;
 
-public interface PhenotypeXDAO extends BaseDAO<PhenotypeX, Long> {
+public interface PhenotypeXDAO extends BaseDAO<PhenotypeX, Integer> {
 
 }
