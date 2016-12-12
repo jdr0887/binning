eb996dfeec18d794aef115e0900fbdce25135527
Jason Reilly <jdr0887@gmail.com>
2016-12-12 15:55:30 -0500

    cleaning

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/model/DiagnosticStatusType.java
+++ binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/model/DiagnosticStatusType.java
@@ -4,8 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import org.renci.binning.dao.Persistable;
