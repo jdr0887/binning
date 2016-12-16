fe8dc0c047f70cc925481358a606bb97f3fdf980
Jason Reilly <jdr0887@gmail.com>
2016-12-16 16:18:57 -0500

    works

--- binning-workflows/binning-workflow-diagnostic-test/binning-workflow-diagnostic-test-commons/src/main/java/org/renci/binning/diagnostic/test/commons/LoadVCFCallable.java
+++ binning-workflows/binning-workflow-diagnostic-test/binning-workflow-diagnostic-test-commons/src/main/java/org/renci/binning/diagnostic/test/commons/LoadVCFCallable.java
@@ -1,19 +1,19 @@
 package org.renci.binning.diagnostic.test.commons;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.renci.binning.core.BinningException;
-import org.renci.binning.core.IRODSUtils;
 import org.renci.binning.core.diagnostic.AbstractLoadVCFCallable;
 import org.renci.binning.dao.BinningDAOBeanService;
 import org.renci.binning.dao.BinningDAOException;
 import org.renci.binning.dao.clinbin.model.DiagnosticBinningJob;
 import org.renci.binning.dao.jpa.BinningDAOManager;
 import org.renci.binning.dao.ref.model.GenomeRef;
+import org.renci.binning.dao.ref.model.GenomeRefSeq;
 import org.renci.binning.dao.var.model.LocatedVariant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,16 +48,28 @@ public class LoadVCFCallable extends AbstractLoadVCFCallable {
     }
 
     @Override
-    public LocatedVariant liftOver(LocatedVariant locatedVariant) throws BinningDAOException {
+    public LocatedVariant liftOver(LocatedVariant locatedVariant) throws BinningException {
         logger.debug("ENTERING liftOver(LocatedVariant)");
-        File chainFile = new File(binningDirectory, "hg19ToHg38.over.chain.gz");
-        GenomeRef build38GenomeRef = getDaoBean().getGenomeRefDAO().findById(4);
-        LiftOver liftOver = new LiftOver(chainFile);
-        Interval interval = new Interval(String.format("chr", locatedVariant.getGenomeRefSeq().getContig()), locatedVariant.getPosition(),
-                locatedVariant.getEndPosition());
-        Interval loInterval = liftOver.liftOver(interval);
-        LocatedVariant ret = new LocatedVariant(build38GenomeRef, locatedVariant.getGenomeRefSeq(), loInterval.getStart(),
-                loInterval.getEnd(), locatedVariant.getVariantType(), locatedVariant.getRef(), locatedVariant.getSeq());
+        LocatedVariant ret;
+        try {
+            File chainFile = new File(binningDirectory, "hg19ToHg38.over.chain.gz");
+            GenomeRef build38GenomeRef = getDaoBean().getGenomeRefDAO().findById(4);
+            LiftOver liftOver = new LiftOver(chainFile);
+            Interval interval = new Interval(String.format("chr%s", locatedVariant.getGenomeRefSeq().getContig()),
+                    locatedVariant.getPosition(), locatedVariant.getEndPosition());
+            Interval loInterval = liftOver.liftOver(interval);
+            List<GenomeRefSeq> genomeRefSeqList = getDaoBean().getGenomeRefSeqDAO().findByRefIdAndContigAndSeqTypeAndAccessionPrefix(
+                    build38GenomeRef.getId(), locatedVariant.getGenomeRefSeq().getContig(), "Chromosome", "NC_");
+
+            if (CollectionUtils.isEmpty(genomeRefSeqList)) {
+                throw new BinningException("GenomeRefSeq not found");
+            }
+
+            ret = new LocatedVariant(build38GenomeRef, genomeRefSeqList.get(0), loInterval.getStart(), loInterval.getEnd(),
+                    locatedVariant.getVariantType(), locatedVariant.getRef(), locatedVariant.getSeq());
+        } catch (Exception e) {
+            throw new BinningException(e);
+        }
         return ret;
     }
 
@@ -71,22 +83,12 @@ public class LoadVCFCallable extends AbstractLoadVCFCallable {
     @Override
     public File getVCF(String participant) throws BinningException {
         logger.debug("ENTERING getVCF(String)");
-        Map<String, String> avuMap = new HashMap<String, String>();
-        avuMap.put("MaPSeqStudyName", "NC_GENES");
-        avuMap.put("MaPSeqWorkflowName", "NCGenesBaseline");
-        avuMap.put("MaPSeqJobName", "GATKApplyRecalibration");
-        avuMap.put("MaPSeqMimeType", "TEXT_VCF");
-        String irodsFile = IRODSUtils.findFile(participant, avuMap);
-        String participantDir = String.format("%s/annotation/test/%s", binningDirectory, participant);
-        File vcfFile = IRODSUtils.getFile(irodsFile, participantDir);
-        return vcfFile;
-    }
-
-    @Override
-    public Void call() throws BinningException {
-        logger.info("ENTERING call()");
-
-        return null;
+        File f = new File(getBinningJob().getVcfFile());
+        logger.info("vcf: {}", f.getAbsolutePath());
+        if (!f.exists()) {
+            throw new BinningException("VCF does not exist");
+        }
+        return f;
     }
 
     public static void main(String[] args) {
