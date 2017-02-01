5807151bb471cd276870fa73871f7d794d33f9c0
Jason Reilly <jdr0887@gmail.com>
2017-02-01 14:19:00 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/annotation/AnnotationGeneDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.binning.dao.annotation;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.annotation.model.AnnotationGene;
-
-public interface AnnotationGeneDAO extends BaseDAO<AnnotationGene, Long> {
-
-}
