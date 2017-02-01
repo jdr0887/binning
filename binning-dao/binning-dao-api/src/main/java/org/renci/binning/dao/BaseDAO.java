08c6241f95a7377f39bc087c05ac9840d8aea35d
Jason Reilly <jdr0887@gmail.com>
2017-02-01 14:19:00 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/BaseDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.binning.dao;
-
-import java.io.Serializable;
-
-public interface BaseDAO<T extends Persistable, ID extends Serializable> {
-
-    public abstract T findById(ID id) throws BinningDAOException;
-
-}
