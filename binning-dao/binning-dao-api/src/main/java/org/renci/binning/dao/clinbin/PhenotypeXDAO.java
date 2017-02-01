6296f134fd0a3feea563680b16413c3fb96ac132
Jason Reilly <jdr0887@gmail.com>
2017-02-01 14:19:00 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/PhenotypeXDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.binning.dao.clinbin;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.clinbin.model.PhenotypeX;
-
-public interface PhenotypeXDAO extends BaseDAO<PhenotypeX, Integer> {
-
-}
