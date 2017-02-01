a0ab90604ca9559f6ab39c9b0efb30485a883b44
Jason Reilly <jdr0887@gmail.com>
2017-02-01 14:19:00 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/CarrierStatusDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.binning.dao.clinbin;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.clinbin.model.CarrierStatus;
-
-public interface CarrierStatusDAO extends BaseDAO<CarrierStatus, Integer> {
-
-}
