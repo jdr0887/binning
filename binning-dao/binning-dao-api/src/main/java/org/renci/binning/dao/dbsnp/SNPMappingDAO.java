89b9d02e65d6128a959727a095df785e7df7fa11
Jason Reilly <jdr0887@gmail.com>
2017-02-01 14:19:01 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/dbsnp/SNPMappingDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.binning.dao.dbsnp;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.dbsnp.model.SNPMapping;
-import org.renci.binning.dao.dbsnp.model.SNPMappingPK;
-
-public interface SNPMappingDAO extends BaseDAO<SNPMapping, SNPMappingPK> {
-
-}
