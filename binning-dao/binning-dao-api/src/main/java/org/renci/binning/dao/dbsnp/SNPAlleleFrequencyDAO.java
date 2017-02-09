5457ee7f880f839db8f8957afee0ee7249f7c0ed
Jason Reilly <jdr0887@gmail.com>
2017-02-09 08:29:45 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/dbsnp/SNPAlleleFrequencyDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.binning.dao.dbsnp;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.dbsnp.model.SNPAlleleFrequency;
-import org.renci.binning.dao.dbsnp.model.SNPAlleleFrequencyPK;
-
-public interface SNPAlleleFrequencyDAO extends BaseDAO<SNPAlleleFrequency, SNPAlleleFrequencyPK> {
-
-}
