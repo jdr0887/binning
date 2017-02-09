db6fd02ef977924bbd770f48a84b09e7ac0af9fc
Jason Reilly <jdr0887@gmail.com>
2017-02-09 08:29:44 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/UnimportantExonDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.binning.dao.clinbin;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.clinbin.model.UnimportantExon;
-import org.renci.binning.dao.clinbin.model.UnimportantExonPK;
-
-public interface UnimportantExonDAO extends BaseDAO<UnimportantExon, UnimportantExonPK> {
-
-}
