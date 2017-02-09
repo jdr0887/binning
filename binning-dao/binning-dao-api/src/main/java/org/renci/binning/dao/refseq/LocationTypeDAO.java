f8eac6b809f38e8954a132bca5f2b3d0afe4998f
Jason Reilly <jdr0887@gmail.com>
2017-02-09 08:29:47 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/refseq/LocationTypeDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.binning.dao.refseq;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.refseq.model.LocationType;
-
-public interface LocationTypeDAO extends BaseDAO<LocationType, String> {
-
-}
