df4fba3e2b80b9d246691b91529b5ed6421336f7
Jason Reilly <jdr0887@gmail.com>
2017-02-09 08:29:44 -0500

    removing

--- binning-dao/binning-dao-api/src/main/java/org/renci/binning/dao/clinbin/MaxFrequencySourceDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.binning.dao.clinbin;
-
-import org.renci.binning.dao.BaseDAO;
-import org.renci.binning.dao.clinbin.model.MaxFrequencySource;
-
-public interface MaxFrequencySourceDAO extends BaseDAO<MaxFrequencySource, String> {
-
-}
