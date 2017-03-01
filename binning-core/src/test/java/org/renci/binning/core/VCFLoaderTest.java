c7fa77b7eb348f93d8d68b85e0a0cfa04d0cad5f
Jason Reilly <jdr0887@gmail.com>
2017-03-01 10:07:51 -0500

    using commons-lang3

--- binning-core/src/test/java/org/renci/binning/core/VCFLoaderTest.java
+++ binning-core/src/test/java/org/renci/binning/core/VCFLoaderTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.renci.canvas.binning.core.BinningException;
 import org.renci.canvas.dao.CANVASDAOException;
