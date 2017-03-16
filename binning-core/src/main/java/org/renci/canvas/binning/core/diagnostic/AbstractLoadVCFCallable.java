54609cb2f70a991fa9360d74ebae0cec7cb741ad
Jason Reilly <jdr0887@gmail.com>
2017-03-16 08:59:27 -0400

    refactor to reduce duplicate attempts to persist into allele_map

--- binning-core/src/main/java/org/renci/canvas/binning/core/diagnostic/AbstractLoadVCFCallable.java
+++ binning-core/src/main/java/org/renci/canvas/binning/core/diagnostic/AbstractLoadVCFCallable.java
@@ -260,7 +260,11 @@ public abstract class AbstractLoadVCFCallable implements Callable<Void> {
                             });
 
                         }
+                        es.shutdown();
+                        es.awaitTermination(1L, TimeUnit.DAYS);
 
+                        es = Executors.newFixedThreadPool(4);
+                        
                         // cant trust htsjdk to parse properly...switch/case on freebayes type (if available)
                         for (VariantContext variantContext : variantContextList) {
                             es.submit(() -> {
