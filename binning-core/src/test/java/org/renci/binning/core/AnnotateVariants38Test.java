2ff26c7aee6e60caaf3fb592275db773077499ca
Jason Reilly <jdr0887@gmail.com>
2017-04-26 15:33:10 -0400

    renamed

---
+++ binning-core/src/test/java/org/renci/binning/core/AnnotateVariants38Test.java
@@ -0,0 +1,351 @@
+package org.renci.binning.core;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.renci.canvas.dao.jpa.CANVASDAOBeanServiceImpl;
+import org.renci.canvas.dao.jpa.annotation.AnnotationGeneExternalIdDAOImpl;
+import org.renci.canvas.dao.jpa.hgnc.HGNCGeneDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.FeatureDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.LocationTypeDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.RefSeqCodingSequenceDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.RefSeqGeneDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.RegionGroupRegionDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.TranscriptMapsDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.TranscriptMapsExonsDAOImpl;
+import org.renci.canvas.dao.jpa.refseq.VariantEffectDAOImpl;
+import org.renci.canvas.dao.jpa.var.LocatedVariantDAOImpl;
+import org.renci.canvas.dao.refseq.model.TranscriptMaps;
+import org.renci.canvas.dao.refseq.model.TranscriptMapsExons;
+import org.renci.canvas.dao.refseq.model.Variants_80_4;
+import org.renci.canvas.dao.var.model.LocatedVariant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnnotateVariants38Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnnotateVariants38Test.class);
+
+    private static EntityManagerFactory emf;
+
+    private static EntityManager em;
+
+    private static CANVASDAOBeanServiceImpl daoBean = new CANVASDAOBeanServiceImpl();
+
+    public AnnotateVariants38Test() {
+        super();
+    }
+
+    @BeforeClass
+    public static void setup() {
+        emf = Persistence.createEntityManagerFactory("canvas_test", null);
+        em = emf.createEntityManager();
+
+        TranscriptMapsDAOImpl transcriptMapsDAO = new TranscriptMapsDAOImpl();
+        transcriptMapsDAO.setEntityManager(em);
+        daoBean.setTranscriptMapsDAO(transcriptMapsDAO);
+
+        TranscriptMapsExonsDAOImpl transcriptMapsExonsDAO = new TranscriptMapsExonsDAOImpl();
+        transcriptMapsExonsDAO.setEntityManager(em);
+        daoBean.setTranscriptMapsExonsDAO(transcriptMapsExonsDAO);
+
+        LocationTypeDAOImpl locationTypeDAO = new LocationTypeDAOImpl();
+        locationTypeDAO.setEntityManager(em);
+        daoBean.setLocationTypeDAO(locationTypeDAO);
+
+        VariantEffectDAOImpl variantEffectDAO = new VariantEffectDAOImpl();
+        variantEffectDAO.setEntityManager(em);
+        daoBean.setVariantEffectDAO(variantEffectDAO);
+
+        RefSeqGeneDAOImpl refSeqGeneDAO = new RefSeqGeneDAOImpl();
+        refSeqGeneDAO.setEntityManager(em);
+        daoBean.setRefSeqGeneDAO(refSeqGeneDAO);
+
+        HGNCGeneDAOImpl hgncGeneDAO = new HGNCGeneDAOImpl();
+        hgncGeneDAO.setEntityManager(em);
+        daoBean.setHGNCGeneDAO(hgncGeneDAO);
+
+        AnnotationGeneExternalIdDAOImpl annotationGeneExternalIdDAO = new AnnotationGeneExternalIdDAOImpl();
+        annotationGeneExternalIdDAO.setEntityManager(em);
+        daoBean.setAnnotationGeneExternalIdDAO(annotationGeneExternalIdDAO);
+
+        RegionGroupRegionDAOImpl regionGroupRegionDAO = new RegionGroupRegionDAOImpl();
+        regionGroupRegionDAO.setEntityManager(em);
+        daoBean.setRegionGroupRegionDAO(regionGroupRegionDAO);
+
+        RefSeqCodingSequenceDAOImpl refSeqCodingSequenceDAO = new RefSeqCodingSequenceDAOImpl();
+        refSeqCodingSequenceDAO.setEntityManager(em);
+        daoBean.setRefSeqCodingSequenceDAO(refSeqCodingSequenceDAO);
+
+        FeatureDAOImpl featureDAO = new FeatureDAOImpl();
+        featureDAO.setEntityManager(em);
+        daoBean.setFeatureDAO(featureDAO);
+
+        LocatedVariantDAOImpl locatedVariantDAO = new LocatedVariantDAOImpl();
+        locatedVariantDAO.setEntityManager(em);
+        daoBean.setLocatedVariantDAO(locatedVariantDAO);
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        em.close();
+        emf.close();
+    }
+
+    private List<Variants_80_4> annotateLocatedVariant(LocatedVariant locatedVariant) throws Exception {
+        List<Variants_80_4> variants = new ArrayList<>();
+
+        org.renci.canvas.binning.core.grch38.VariantsFactory variantsFactory = org.renci.canvas.binning.core.grch38.VariantsFactory
+                .getInstance();
+
+        List<TranscriptMaps> transcriptMapsList = daoBean.getTranscriptMapsDAO()
+                .findByGenomeRefIdAndRefSeqVersionAndGenomeRefSeqAccessionAndInExonRange(4, "80", locatedVariant.getGenomeRefSeq().getId(),
+                        locatedVariant.getPosition());
+        logger.info("transcriptMapsList.size(): {}", transcriptMapsList.size());
+
+        if (CollectionUtils.isNotEmpty(transcriptMapsList)) {
+            // handling non boundary crossing variants (intron/exon/utr*)
+
+            transcriptMapsList.sort((a, b) -> b.getTranscript().getId().compareTo(a.getTranscript().getId()));
+
+            for (TranscriptMaps tMap : transcriptMapsList) {
+
+                List<TranscriptMapsExons> transcriptMapsExonsList = daoBean.getTranscriptMapsExonsDAO()
+                        .findByTranscriptMapsId(tMap.getId());
+
+                List<TranscriptMaps> mapsList = daoBean.getTranscriptMapsDAO().findByGenomeRefIdAndRefSeqVersionAndTranscriptId(2, "80",
+                        tMap.getTranscript().getId());
+
+                Variants_80_4 variant = null;
+
+                Optional<TranscriptMapsExons> optionalTranscriptMapsExons = transcriptMapsExonsList.parallelStream()
+                        .filter(a -> a.getContigRange().contains(locatedVariant.getPosition())).findAny();
+                if (optionalTranscriptMapsExons.isPresent()) {
+                    TranscriptMapsExons transcriptMapsExons = optionalTranscriptMapsExons.get();
+                    logger.info(transcriptMapsExons.toString());
+
+                    if ((transcriptMapsExons.getContigEnd().equals(locatedVariant.getPosition()) && "-".equals(tMap.getStrand()))
+                            || (transcriptMapsExons.getContigStart().equals(locatedVariant.getPosition())
+                                    && "+".equals(tMap.getStrand()))) {
+                        variant = variantsFactory.createBorderCrossingVariant(daoBean, locatedVariant, tMap, mapsList,
+                                transcriptMapsExonsList, transcriptMapsExons);
+                    } else {
+                        variant = variantsFactory.createExonicVariant(daoBean, locatedVariant, mapsList, transcriptMapsExonsList,
+                                transcriptMapsExons);
+                    }
+
+                } else {
+                    variant = variantsFactory.createIntronicVariant(daoBean, locatedVariant, mapsList, tMap, transcriptMapsExonsList);
+                }
+                variants.add(variant);
+
+            }
+
+        } else {
+
+            // try searching by adjusting for length of locatedVariant.getSeq()...could be intron/exon boundary crossing
+            transcriptMapsList = daoBean.getTranscriptMapsDAO().findByGenomeRefIdAndRefSeqVersionAndGenomeRefSeqAccessionAndInExonRange(4,
+                    "80", locatedVariant.getGenomeRefSeq().getId(), locatedVariant.getPosition() + locatedVariant.getRef().length() - 1);
+
+            if (CollectionUtils.isNotEmpty(transcriptMapsList)) {
+
+                transcriptMapsList.sort((a, b) -> b.getTranscript().getId().compareTo(a.getTranscript().getId()));
+
+                for (TranscriptMaps tMap : transcriptMapsList) {
+
+                    List<TranscriptMapsExons> transcriptMapsExonsList = daoBean.getTranscriptMapsExonsDAO()
+                            .findByTranscriptMapsId(tMap.getId());
+
+                    Optional<TranscriptMapsExons> optionalTranscriptMapsExons = transcriptMapsExonsList.parallelStream()
+                            .filter(a -> a.getContigRange().contains(locatedVariant.getPosition() + locatedVariant.getRef().length() - 1))
+                            .findAny();
+                    if (optionalTranscriptMapsExons.isPresent()) {
+                        TranscriptMapsExons transcriptMapsExons = optionalTranscriptMapsExons.get();
+                        logger.info(transcriptMapsExons.toString());
+                        List<TranscriptMaps> mapsList = daoBean.getTranscriptMapsDAO().findByGenomeRefIdAndRefSeqVersionAndTranscriptId(4,
+                                "80", tMap.getTranscript().getId());
+                        Variants_80_4 variant = variantsFactory.createBorderCrossingVariant(daoBean, locatedVariant, tMap, mapsList,
+                                transcriptMapsExonsList, transcriptMapsExons);
+                        variants.add(variant);
+                    }
+
+                }
+            } else {
+                transcriptMapsList = daoBean.getTranscriptMapsDAO().findByGenomeRefIdAndRefSeqVersionAndGenomeRefSeqAccessionAndInExonRange(
+                        2, "80", locatedVariant.getGenomeRefSeq().getId(), locatedVariant.getPosition() - locatedVariant.getRef().length());
+                if (CollectionUtils.isNotEmpty(transcriptMapsList)) {
+
+                    transcriptMapsList.sort((a, b) -> b.getTranscript().getId().compareTo(a.getTranscript().getId()));
+
+                    for (TranscriptMaps tMap : transcriptMapsList) {
+
+                        List<TranscriptMapsExons> transcriptMapsExonsList = daoBean.getTranscriptMapsExonsDAO()
+                                .findByTranscriptMapsId(tMap.getId());
+
+                        Optional<TranscriptMapsExons> optionalTranscriptMapsExons = transcriptMapsExonsList.parallelStream()
+                                .filter(a -> a.getContigRange().contains(locatedVariant.getPosition() - locatedVariant.getRef().length()))
+                                .findAny();
+                        if (optionalTranscriptMapsExons.isPresent()) {
+                            TranscriptMapsExons transcriptMapsExons = optionalTranscriptMapsExons.get();
+                            logger.info(transcriptMapsExons.toString());
+                            List<TranscriptMaps> mapsList = daoBean.getTranscriptMapsDAO()
+                                    .findByGenomeRefIdAndRefSeqVersionAndTranscriptId(4, "80", tMap.getTranscript().getId());
+                            Variants_80_4 variant = variantsFactory.createBorderCrossingVariant(daoBean, locatedVariant, tMap, mapsList,
+                                    transcriptMapsExonsList, transcriptMapsExons);
+                            variants.add(variant);
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        return variants;
+    }
+
+    @Test
+    public void testLocatedVariant588216920() throws Exception {
+
+        LocatedVariant locatedVariant = daoBean.getLocatedVariantDAO().findById(588216920L);
+        logger.info(locatedVariant.toString());
+
+        List<Variants_80_4> variants = annotateLocatedVariant(locatedVariant);
+        assertTrue(variants.size() == 1);
+
+        Variants_80_4 variant = variants.get(0);
+        logger.info(variant.toString());
+        // assertTrue(variant.getId().getMapNumber().equals(1));
+        assertTrue(variant.getNumberOfTranscriptMaps().equals(1));
+        assertTrue(variant.getLocatedVariant().getId().equals(588216920L));
+        assertTrue(variant.getGenomeRefSeq().getId().equals("NC_000017.10"));
+        assertTrue(variant.getLocatedVariant().getPosition().equals(59529156));
+        assertTrue(variant.getId().getTranscript().equals("XM_005257838.1"));
+        assertTrue(variant.getId().getVariantEffect().equals("splice-site-UTR-5"));
+        assertTrue(variant.getVariantType().getId().equals("snp"));
+        assertTrue(variant.getReferenceAllele().equals("T"));
+        assertTrue(variant.getAlternateAllele().equals("A"));
+        assertTrue(variant.getGene().getId().equals(29997));
+        assertTrue(variant.getRefSeqGene().equals("TBX4"));
+        assertTrue(variant.getHgncGene().equals("TBX4"));
+        assertTrue(variant.getLocationType().getId().equals("intron"));
+        assertTrue(variant.getStrand().equals("+"));
+        assertTrue(variant.getIntronExonDistance().equals(2));
+        assertTrue(variant.getHgvsGenomic().equals("NC_000017.10:g.59529156T>A"));
+    }
+
+    @Test
+    public void testLocatedVariant562476950() throws Exception {
+
+        LocatedVariant locatedVariant = daoBean.getLocatedVariantDAO().findById(562476950L);
+        logger.info(locatedVariant.toString());
+
+        List<Variants_80_4> variants = annotateLocatedVariant(locatedVariant);
+        assertTrue(variants.size() == 1);
+
+        Variants_80_4 variant = variants.get(0);
+        logger.info(variant.toString());
+        // assertTrue(variant.getId().getMapNumber().equals(1));
+        // assertTrue(variant.getNumberOfTranscriptMaps().equals(1));
+        assertTrue(variant.getLocatedVariant().getId().equals(562476950L));
+        assertTrue(variant.getGenomeRefSeq().getId().equals("NC_000001.11"));
+        assertTrue(variant.getLocatedVariant().getPosition().equals(12920412));
+        assertTrue(variant.getId().getTranscript().equals("NM_001012277.4"));
+        assertTrue(variant.getId().getVariantEffect().equals("synonymous"));
+        assertTrue(variant.getVariantType().getId().equals("snp"));
+        assertTrue(variant.getReferenceAllele().equals("G"));
+        assertTrue(variant.getAlternateAllele().equals("A"));
+        assertTrue(variant.getGene().getId().equals(22694));
+        assertTrue(variant.getRefSeqGene().equals("PRAMEF7"));
+        assertTrue(variant.getHgncGene().equals("PRAMEF7"));
+        assertTrue(variant.getLocationType().getId().equals("exon"));
+        assertTrue(variant.getStrand().equals("+"));
+        assertTrue(variant.getIntronExonDistance().equals(561));
+        assertTrue(variant.getHgvsGenomic().equals("NC_000001.11:g.12920412G>A"));
+        assertTrue(variant.getHgvsCodingSequence().equals("NM_001012277.4:c.1424G>A"));
+        assertTrue(variant.getHgvsTranscript().equals("NM_001012277.4:g.1490G>A"));
+
+    }
+
+    @Test
+    public void testLocatedVariant532811053() throws Exception {
+
+        LocatedVariant locatedVariant = daoBean.getLocatedVariantDAO().findById(532811053L);
+        logger.info(locatedVariant.toString());
+
+        List<Variants_80_4> variants = annotateLocatedVariant(locatedVariant);
+        assertTrue(variants.size() == 2);
+
+        Variants_80_4 variant = variants.get(0);
+        logger.info(variant.toString());
+        // assertTrue(variant.getId().getMapNumber().equals(1));
+        // assertTrue(variant.getNumberOfTranscriptMaps().equals(1));
+        assertTrue(variant.getLocatedVariant().getId().equals(532811053L));
+        assertTrue(variant.getGenomeRefSeq().getId().equals("NC_000001.11"));
+        assertTrue(variant.getLocatedVariant().getPosition().equals(77558665));
+        assertTrue(variant.getId().getTranscript().equals("NM_174858.2"));
+        assertTrue(variant.getId().getVariantEffect().equals("non-frameshifting indel"));
+        assertTrue(variant.getVariantType().getId().equals("ins"));
+        assertTrue(variant.getReferenceAllele().equals(""));
+        assertTrue(variant.getAlternateAllele().equals("ATT"));
+        assertTrue(variant.getGene().getId().equals(646));
+        assertTrue(variant.getRefSeqGene().equals("AK5"));
+        assertTrue(variant.getHgncGene().equals("AK5"));
+        assertTrue(variant.getLocationType().getId().equals("exon"));
+        assertTrue(variant.getStrand().equals("+"));
+        assertTrue(variant.getIntronExonDistance().equals(64));
+        assertTrue(variant.getCodingSequencePosition().equals(1684));
+        assertTrue(variant.getAminoAcidStart().equals(562));
+        assertTrue(variant.getAminoAcidEnd().equals(563));
+        assertTrue(variant.getOriginalAminoAcid().equals("F"));
+        assertTrue(variant.getFinalAminoAcid().equals("IF"));
+        assertTrue(variant.getTranscriptPosition().equals(2021));
+        assertTrue(variant.getHgvsGenomic().equals("NC_000001.11:g.77558665_77558666insATT"));
+        assertTrue(variant.getHgvsCodingSequence().equals("NM_174858.2:c.1684_1685insATT"));
+        assertTrue(variant.getHgvsTranscript().equals("NM_174858.2:g.2021_2022insATT"));
+        assertTrue(variant.getHgvsProtein().equals("NP_777283.1:p.Phe562delinsIlePhe"));
+
+        variant = variants.get(1);
+        logger.info(variant.toString());
+        // assertTrue(variant.getId().getMapNumber().equals(1));
+        // assertTrue(variant.getNumberOfTranscriptMaps().equals(1));
+        assertTrue(variant.getLocatedVariant().getId().equals(532811053L));
+        assertTrue(variant.getGenomeRefSeq().getId().equals("NC_000001.11"));
+        assertTrue(variant.getLocatedVariant().getPosition().equals(77558665));
+        assertTrue(variant.getId().getTranscript().equals("NM_012093.3"));
+        assertTrue(variant.getId().getVariantEffect().equals("non-frameshifting indel"));
+        assertTrue(variant.getVariantType().getId().equals("ins"));
+        assertTrue(variant.getReferenceAllele().equals(""));
+        assertTrue(variant.getAlternateAllele().equals("ATT"));
+        assertTrue(variant.getGene().getId().equals(646));
+        assertTrue(variant.getRefSeqGene().equals("AK5"));
+        assertTrue(variant.getHgncGene().equals("AK5"));
+        assertTrue(variant.getLocationType().getId().equals("exon"));
+        assertTrue(variant.getStrand().equals("+"));
+        assertTrue(variant.getIntronExonDistance().equals(64));
+        assertTrue(variant.getCodingSequencePosition().equals(1606));
+        assertTrue(variant.getAminoAcidStart().equals(536));
+        assertTrue(variant.getAminoAcidEnd().equals(537));
+        assertTrue(variant.getOriginalAminoAcid().equals("F"));
+        assertTrue(variant.getFinalAminoAcid().equals("IF"));
+        assertTrue(variant.getTranscriptPosition().equals(2632));
+        assertTrue(variant.getHgvsGenomic().equals("NC_000001.11:g.77558665_77558666insATT"));
+        assertTrue(variant.getHgvsCodingSequence().equals("NM_012093.3:c.1606_1607insATT"));
+        assertTrue(variant.getHgvsTranscript().equals("NM_012093.3:g.2632_2633insATT"));
+        assertTrue(variant.getHgvsProtein().equals("NP_036225.2:p.Phe536delinsIlePhe"));
+
+    }
+
+}
