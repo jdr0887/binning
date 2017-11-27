cc886016c399629de6c8fdf4f72d39cdd8e5f92c
Jason Reilly <jdr0887@gmail.com>
2017-11-27 09:31:14 -0500

    adding test to demonstrate that closes #10

--- binning-core/src/test/java/org/renci/binning/core/AnnotateVariants38InsTest.java
+++ binning-core/src/test/java/org/renci/binning/core/AnnotateVariants38InsTest.java
@@ -256,6 +256,38 @@ public class AnnotateVariants38InsTest extends AbstractAnnotateVariants38Test {
     }
 
     @Test
+    public void testLocatedVariant595706057() throws Exception {
+
+        LocatedVariant locatedVariant = daoBean.getLocatedVariantDAO().findById(595706057L);
+        logger.info(locatedVariant.toString());
+
+        List<Variants_80_4> variants = annotateLocatedVariant(locatedVariant);
+
+        Variants_80_4 variant = variants.stream().filter(a -> a.getId().getTranscript().equals("NM_002473.5")).findFirst().get();
+        logger.info(variant.toString());
+
+        assertTrue(variant.getLocatedVariant().getId().equals(595706057L));
+        assertTrue(variant.getGenomeRefSeq().getId().equals("NC_000022.11"));
+        assertTrue(variant.getLocatedVariant().getPosition().equals(36289295));
+        assertTrue(variant.getVariantType().getId().equals("sub"));
+        assertTrue(variant.getId().getTranscript().equals("NM_002473.5"));
+        assertTrue(variant.getRefSeqGene().equals("MYH9"));
+        assertTrue(variant.getHgncGene().equals("MYH9"));
+        assertTrue(variant.getLocationType().getId().equals("intron/exon boundary"));
+        assertTrue(variant.getStrand().equals("-"));
+        assertTrue(variant.getIntronExonDistance().equals(0));
+        assertTrue(variant.getId().getVariantEffect().equals("boundary-crossing indel"));
+        assertTrue(variant.getGene().getId().equals(18264));
+        assertTrue(variant.getReferenceAllele().equals("GAGCTG"));
+        assertTrue(variant.getAlternateAllele().equals("AA"));
+        assertTrue(variant.getHgvsGenomic().equals("NC_000022.11:g.36289295_36289300delinsAA"));
+        assertTrue(variant.getHgvsCodingSequence().equals("?"));
+        assertTrue(variant.getHgvsTranscript().equals("?"));
+        assertTrue(variant.getHgvsProtein().equals("?"));
+
+    }
+
+    @Test
     public void testLocatedVariant522389045() throws Exception {
 
         LocatedVariant locatedVariant = daoBean.getLocatedVariantDAO().findById(522389045L);
